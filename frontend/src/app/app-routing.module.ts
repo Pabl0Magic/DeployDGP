ffab7fdaed035eb5ac30e3e50f3907081059eeb2
Javier Ye <javieryezhang@gmail.com>
2023-10-10 21:35:53 +0200

    #14 Añadida interfaz básica de salas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { NewSalaComponent } from './components/dashboard/sala/new-sala/new-sala.component';
-import { SalaComponent } from './components/dashboard/sala/sala.component';
+import { NewSalaComponent } from './components/dashboard/new-sala/new-sala.component';
 import { SalaImportComponent } from './components/dashboard/sala/sala-import/sala-import.component';
 import { OverviewComponent } from './components/dashboard/sala/overview/overview.component';
-import { ModifySalaComponent } from './components/dashboard/sala/modify-sala/modify-sala.component';
+import { ModifySalaComponent } from './components/dashboard/modify-sala/modify-sala.component';
+import { SalaComponent } from './components/dashboard/sala/sala.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -26,15 +26,15 @@ const routes: Routes = [
         component: SalaImportComponent
       },
       {
-        path: ':salaId/modify-sala',
-        component: ModifySalaComponent
-      },
-      {
-        path: ':salaId/overview',
-        component: OverviewComponent,
+        path: 'sala/:salaId',
+        component: SalaComponent,
         children: [
           {
-            path: 'modify-sala',
+            path: 'overview',
+            component: OverviewComponent,
+          },
+          {
+            path: ':modify-sala',
             component: ModifySalaComponent
           }
         ]
