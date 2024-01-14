edce06243502f8fbc7bedf09d1b1fbb3dabedaf6
Mimoci12 <miguel.mc.uam@gmail.com>
2024-01-14 21:19:19 +0100

    #60 Mejora test Sala

--- backend/heroku-django-template-master/project/tests/test_views_sala.py
+++ backend/heroku-django-template-master/project/tests/test_views_sala.py
@@ -47,6 +47,24 @@ class SalaTests(TestCase):
 
         self.assertEqual(response.status_code, 201)
 
+        response = self.client.post(
+            "/project/room/create/", {"name": "SALA", "size": 60}
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.post(
+            "/project/room/create/", {"nam": "SALA", "size": 60}
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.post(
+            "/project/room/create/", {"name": "SALA", "siz": 60}
+        )
+
+        self.assertEqual(response.status_code, 400)
+
     def test_room_get(self):
         """Test for room get"""
 
@@ -60,8 +78,16 @@ class SalaTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        response = self.client.get("/project/room/SALA1/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_room_delete(self):
         """Test for room delete"""
+
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 404)
 
         Room.objects.create(name="SALA", size=60)
 
@@ -69,8 +95,20 @@ class SalaTests(TestCase):
 
         self.assertEqual(response.status_code, 204)
 
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_room_patch(self):
         """Test for room patch"""
+
+        response = self.client.patch(
+            "/project/room/SALA/",
+            {"name": "SALA1", "size": 80},
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 404)
 
         Room.objects.create(name="SALA", size=60)
 
