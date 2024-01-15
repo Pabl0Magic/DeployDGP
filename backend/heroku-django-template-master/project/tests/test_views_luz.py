1e8c4a50c625b682ff0ccdc96061a42f5d73e5ed
Pabl0Magic <pabloalmarzamarques@gmail.com>
2024-01-15 19:32:38 +0100

    #64 Casos de error para luz

--- backend/heroku-django-template-master/project/tests/test_views_luz.py
+++ backend/heroku-django-template-master/project/tests/test_views_luz.py
@@ -34,6 +34,18 @@ class LuzTests(TestCase):
 
         self.assertEqual(response.status_code, 201)
 
+        response = self.client.post(
+            "/project/room/SALA/light/create/", {"badname": "SALA"}
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.post(
+            "/project/room/SALA/light/create/", {"room": "CREATENOTHERE"}
+        )
+
+        self.assertEqual(response.status_code, 400)
+
     def test_light_get(self):
         """Test for light get"""
 
@@ -47,8 +59,18 @@ class LuzTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        Light.objects.filter(id=1).delete()
+
+        response = self.client.get("/project/room/SALA/light/1/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_light_delete(self):
         """Test for light delete"""
+
+        response = self.client.delete("/project/room/SALA/light/1/")
+
+        self.assertEqual(response.status_code, 404)        
 
         Light.objects.create(id=1, room=Room.objects.get(name="SALA"))
 
@@ -56,8 +78,18 @@ class LuzTests(TestCase):
 
         self.assertEqual(response.status_code, 204)
 
+        response = self.client.delete("/project/room/SALA/light/1/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_light_patch(self):
         """Test for light patch"""
+
+        response = self.client.patch(
+            "/project/room/SALA/light/1/", {"id": "2"}, content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 404)
 
         Light.objects.create(id=1, room=Room.objects.get(name="SALA"))
 
@@ -70,17 +102,37 @@ class LuzTests(TestCase):
     def test_light_add_ts(self):
         """Test for light timestamp add"""
 
+        response = self.client.post("/project/room/SALA/light/1/addTs/")
+
+        self.assertEqual(response.status_code, 404)
+
         Light.objects.create(id=1, room=Room.objects.get(name="SALA"))
 
         response = self.client.post("/project/room/SALA/light/1/addTs/")
 
         self.assertEqual(response.status_code, 201)
 
+        Light.objects.filter(id=1).delete()
+
+        response = self.client.post("/project/room/SALA/light/1/addTs/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_light_activity(self):
         """Test for light activity"""
+
+        response = self.client.get("/project/room/SALA/window/1/activity/")
+
+        self.assertEqual(response.status_code, 500)
 
         Light.objects.create(id=1, room=Room.objects.get(name="SALA"))
 
         response = self.client.get("/project/room/SALA/light/1/activity/")
 
         self.assertEqual(response.status_code, 200)
+
+        Light.objects.filter(id=1).delete()
+
+        response = self.client.get("/project/room/SALA/window/1/activity/")
+
+        self.assertEqual(response.status_code, 500)
