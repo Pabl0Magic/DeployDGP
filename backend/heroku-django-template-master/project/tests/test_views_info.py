c78f9b77ede1077932f92585dc566cf75a4000ad
Pabl0Magic <pabloalmarzamarques@gmail.com>
2024-01-15 19:05:51 +0100

    #63 Casos de error para test info

--- backend/heroku-django-template-master/project/tests/test_views_info.py
+++ backend/heroku-django-template-master/project/tests/test_views_info.py
@@ -26,6 +26,14 @@ class InfoTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 204)
+
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_room_last_10_people(self):
         "Test for room last 10 people"
 
@@ -38,6 +46,15 @@ class InfoTests(TestCase):
         response = self.client.get("/project/room/SALA/people/last10/")
 
         self.assertEqual(response.status_code, 200)
+
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 204)
+
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 404)
+
 
     def test_room_add_people(self):
         "Test for room add people"
@@ -54,6 +71,14 @@ class InfoTests(TestCase):
         room_people = RoomPeople.objects.get(room=Room.objects.get(name="SALA"))
         self.assertEqual(room_people.people, 15)
 
+        response = self.client.post(
+            "/project/room/NOEXISTE/people/add/",
+            data=data,
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+
     def test_room_temperature(self):
         "Test for room temperature"
 
@@ -69,6 +94,14 @@ class InfoTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 204)
+
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_room_last_10_temperature(self):
         "Test for room last 10 temperature"
 
@@ -83,6 +116,14 @@ class InfoTests(TestCase):
         response = self.client.get("/project/room/SALA/temperature/last10/")
 
         self.assertEqual(response.status_code, 200)
+
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 204)
+
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 404)
 
     def test_room_add_temperature(self):
         "Test for room add temperature"
@@ -101,6 +142,14 @@ class InfoTests(TestCase):
         )
         self.assertEqual(room_temperature.temperature, 15)
 
+        # Forzar 404
+        response = self.client.post(
+            "/project/room/NOEXISTE/temperature/add/",
+            data=data,
+        )
+
+        self.assertEqual(response.status_code, 404)
+
     def test_room_co2(self):
         "Test for room co2"
 
@@ -114,6 +163,14 @@ class InfoTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 204)
+
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_room_last_10_co2(self):
         "Test for room last 10 co2"
 
@@ -126,6 +183,14 @@ class InfoTests(TestCase):
         response = self.client.get("/project/room/SALA/co2/last10/")
 
         self.assertEqual(response.status_code, 200)
+
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 204)
+
+        response = self.client.delete("/project/room/SALA/")
+
+        self.assertEqual(response.status_code, 404)
 
     def test_room_add_co2(self):
         "Test for room add co2"
@@ -146,3 +211,11 @@ class InfoTests(TestCase):
         # Check if RoomCO2 object is created with the correct values
         room_co2 = RoomCO2.objects.get(room=Room.objects.get(name="SALA"))
         self.assertEqual(room_co2.co2, 15)
+
+        # Forzar 404
+        response = self.client.post(
+            "/project/room/NOEXISTE/co2/add/",
+            data=data,
+        )
+
+        self.assertEqual(response.status_code, 404)
