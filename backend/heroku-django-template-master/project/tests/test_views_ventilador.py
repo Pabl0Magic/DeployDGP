991133f978efb935f50455c2e6da9f7ec52e37d2
Mimoci12 <miguel.mc.uam@gmail.com>
2024-01-15 18:55:40 +0100

    #62 Mejora Test Ventilador

--- backend/heroku-django-template-master/project/tests/test_views_ventilador.py
+++ backend/heroku-django-template-master/project/tests/test_views_ventilador.py
@@ -25,6 +25,12 @@ class VentiladorTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        Ventilator.objects.filter(id=1).delete()
+
+        response = self.client.get("/project/room/SALA/ventilator/all/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_ventilator_create(self):
         """Test for ventilator create"""
 
@@ -33,6 +39,18 @@ class VentiladorTests(TestCase):
         )
 
         self.assertEqual(response.status_code, 201)
+
+        response = self.client.post(
+            "/project/room/SALA/ventilator/create/", {"roo": "SALA"}
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.post(
+            "/project/room/SALA/ventilator/create/", {"room": "SALA2"}
+        )
+
+        self.assertEqual(response.status_code, 400)
 
     def test_ventilator_get(self):
         """Test for ventilator get"""
@@ -47,8 +65,18 @@ class VentiladorTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+        Ventilator.objects.filter(id=1).delete()
+
+        response = self.client.get("/project/room/SALA/ventilator/1/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_ventilator_delete(self):
         """Test for ventilator delete"""
+
+        response = self.client.delete("/project/room/SALA/ventilator/1/")
+
+        self.assertEqual(response.status_code, 404)
 
         Ventilator.objects.create(id=1, room=Room.objects.get(name="SALA"))
 
@@ -56,8 +84,20 @@ class VentiladorTests(TestCase):
 
         self.assertEqual(response.status_code, 204)
 
+        response = self.client.delete("/project/room/SALA/ventilator/1/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_ventilator_patch(self):
         """Test for ventilator patch"""
+
+        response = self.client.patch(
+            "/project/room/SALA/ventilator/1/",
+            {"id": "2"},
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 404)
 
         Ventilator.objects.create(id=1, room=Room.objects.get(name="SALA"))
 
@@ -71,6 +111,10 @@ class VentiladorTests(TestCase):
 
     def test_ventilator_add_ts(self):
         """Test for ventilator timestamp add"""
+        
+        response = self.client.post("/project/room/SALA/ventilator/1/addTs/")
+
+        self.assertEqual(response.status_code, 404)
 
         Ventilator.objects.create(id=1, room=Room.objects.get(name="SALA"))
 
@@ -78,11 +122,29 @@ class VentiladorTests(TestCase):
 
         self.assertEqual(response.status_code, 201)
 
+        Ventilator.objects.filter(id=1).delete()
+
+        response = self.client.post("/project/room/SALA/ventilator/1/addTs/")
+
+        self.assertEqual(response.status_code, 404)
+
     def test_ventilator_activity(self):
         """Test for ventilator activity"""
+
+        response = self.client.get("/project/room/SALA/ventilator/1/activity/")
+
+        self.assertEqual(response.status_code, 500)
 
         Ventilator.objects.create(id=1, room=Room.objects.get(name="SALA"))
 
         response = self.client.get("/project/room/SALA/ventilator/1/activity/")
 
         self.assertEqual(response.status_code, 200)
+
+        Ventilator.objects.filter(id=1).delete()
+
+        response = self.client.get("/project/room/SALA/ventilator/1/activity/")
+
+        self.assertEqual(response.status_code, 500)
+
+
